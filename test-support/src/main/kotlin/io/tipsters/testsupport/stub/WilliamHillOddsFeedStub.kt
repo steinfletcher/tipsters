a8d3e427942fe05505711eae66254e59f7ef0312
stein <steinfletcher@gmail.com>
2016-04-13 23:41:19 +0100

    clean up

--- test-support/src/main/kotlin/io/tipsters/testsupport/stub/WilliamHillOddsFeedStub.kt
+++ test-support/src/main/kotlin/io/tipsters/testsupport/stub/WilliamHillOddsFeedStub.kt
@@ -9,16 +9,9 @@ import com.google.common.io.Resources.toString
 import java.util.concurrent.Executors
 import kotlin.text.Charsets.UTF_8
 
-val server = {
-    val pafApiStub = WilliamHillOddsFeedStub(19002)
-    pafApiStub.start()
-    pafApiStub.willReturnTheOddsXMLFeed()
-}
-
-fun main(args: Array<String>) {
-    Executors.newSingleThreadExecutor().execute(server)
-}
-
+/**
+ * Wiremock stub of the william hill api. Used in integration tests
+ */
 class WilliamHillOddsFeedStub constructor(val port: Int) {
     private val mockServer = WireMockServer(wireMockConfig().port(port))
     private lateinit var wireMock: WireMock
@@ -43,4 +36,17 @@ class WilliamHillOddsFeedStub constructor(val port: Int) {
                         .withHeader("Content-Type", "application/xml")
                         .withBody(xmlResponse)))
     }
+}
+
+/**
+ * Runner that allows the stub to be invoked manually
+ */
+val server = {
+    val williamHillStub = WilliamHillOddsFeedStub(19002)
+    williamHillStub.start()
+    williamHillStub.willReturnTheOddsXMLFeed()
+}
+
+fun main(args: Array<String>) {
+    Executors.newSingleThreadExecutor().execute(server)
 }
