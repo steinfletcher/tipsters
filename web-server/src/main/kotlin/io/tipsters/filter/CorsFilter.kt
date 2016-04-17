84afe631c6de75e1a5c3252e13f1d842135d482a
stein <steinfletcher@gmail.com>
2016-04-17 21:38:50 +0100

    Update CORS config

--- web-server/src/main/kotlin/io/tipsters/filter/CorsFilter.kt
+++ web-server/src/main/kotlin/io/tipsters/filter/CorsFilter.kt
@@ -1,11 +1,9 @@
 package io.tipsters.filter
 
 import org.springframework.stereotype.Component
-
 import javax.servlet.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import java.io.IOException
 
 @Component
 class CorsFilter : Filter {
@@ -14,14 +12,13 @@ class CorsFilter : Filter {
     override fun init(filterConfig: FilterConfig) {
     }
 
-    @Throws(IOException::class, ServletException::class)
     override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
         val request = req as HttpServletRequest
         val response = res as HttpServletResponse
         val requestOrigin = request.getHeader("Origin")
-        response.setHeader("Access-Control-Allow-Origin", requestOrigin)
+        response.setHeader("Access-Control-Allow-Origin", "*")
         response.setHeader("Access-Control-Allow-Credentials", "true")
-        response.setHeader("Access-Control-Allow-Methods", "GET")
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
         response.setHeader("Access-Control-Max-Age", "3600")
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, X-Requested-With")
 
