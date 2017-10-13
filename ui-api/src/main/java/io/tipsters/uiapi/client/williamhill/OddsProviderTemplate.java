db248e235fa7365a7557ff60a36a1dd72c8bd3a1
Stein Fletcher <steinfletcher@gmail.com>
2017-10-13 20:57:09 +0100

    Cache last success if api down

--- ui-api/src/main/java/io/tipsters/uiapi/client/williamhill/OddsProviderTemplate.java
+++ ui-api/src/main/java/io/tipsters/uiapi/client/williamhill/OddsProviderTemplate.java
@@ -1,24 +1,29 @@
 package io.tipsters.uiapi.client.williamhill;
 
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
+import static java.util.stream.Collectors.toList;
 
 import io.tipsters.uiapi.dto.CompetitionMatches;
 import io.tipsters.uiapi.web.OddsProviderError;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import okhttp3.ResponseBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
 import retrofit2.Response;
 
-import static java.util.stream.Collectors.toList;
-
 public abstract class OddsProviderTemplate implements OddsProvider {
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  private List<CompetitionMatches> lastSuccess = Collections.emptyList();
+
   @Override
   public List<CompetitionMatches> odds(Set<String> competitionNames, LocalDateTime matchStart, LocalDateTime matchEnd) {
     try {
@@ -29,14 +34,18 @@ public abstract class OddsProviderTemplate implements OddsProvider {
         parser.parse(response.body().byteStream(), handler);
         List<CompetitionMatches> matches = handler.getCompetitions();
 
-        return matches.stream()
+        List<CompetitionMatches> competitionMatches = matches.stream()
             .filter(competition -> competitionNames.contains(competition.getCompetition()))
             .collect(toList());
+
+        lastSuccess = competitionMatches;
+        return competitionMatches;
       } else {
         throw new OddsProviderError("Failed to retrieve odds from William Hill API");
       }
     } catch (ParserConfigurationException | SAXException | IOException e) {
-      throw new OddsProviderError("Failed to parse Odds from william hill", e);
+      log.error("Failed to parse Odds from william hill. Returning cached response from last succes", e);
+      return lastSuccess;
     }
   }
 
